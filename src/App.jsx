d23dec49db9de7d0329e8feccc11a7b73c4d7321
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-07-09 15:59:01 +0530

    scrolling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -50,11 +50,9 @@ function App() {
           </div>
           <div className="nav-profile">
             <nav className="nav desktop-nav">
-        
               <Link to="/about" className={`nav-content ${nav==="about"? "selected":""}`} onClick={()=>setNav("about")}
               >About</Link>
                <Link to='/AI-Divie' className={`nav-content ${nav==="AI-Divie"? "selected":"none"}`} onClick={()=>setNav("AI-Divie")} >AI-Divie</Link>
-              {/* <Link to="/e-sarthee" className={`nav-content ${nav==="e-sarthee"? "selected":"none"}`} onClick={()=>setNav("e-sarthee")} >E-Sarthee</Link> */}
               <a href="https://esarthee.vercel.app/"  className={`nav-content ${nav==="esarthee"? "selected":"none"}`} onClick={()=>setNav("esarthee")} >E-Sarthee</a>
               <a href="https://p-tunes.vercel.app/"  className={`nav-content ${nav==="p-tunes"? "selected":"none"}`} onClick={()=>setNav("p-tunes")} >P-Tunes</a>
               <Link to='/contact' className={`nav-content ${nav==="contact"? "selected":"none"}`} onClick={()=>setNav("contact")} >Contact</Link>
@@ -63,16 +61,32 @@ function App() {
             <img src={myData.profilePic} alt="Profile" className="profile" />
           </div>
         </header>
-        <nav className="nav-second mobile-nav">
 
-              <Link to="/about" className={`second-nav-content ${nav==="about"?"selected":""}`} onClick={()=>setNav("about")}>About</Link>
-              <Link to='/about' className={`second-nav-content ${nav==="hello"?"selected":""}`} onClick={()=>window.open("https://ai-divie-purush-gurjar.vercel.app","_self")} >AI-Divie</Link>
-              {/* <Link to="/e-sarthee" className={`second-nav-content ${nav==="e-sarthee"?"selected":""}`} onClick={()=>setNav("e-sarthee")} >E-Sarthee</Link> */}
-              <a href="https://esarthee.vercel.app/"  className={`second-nav-content ${nav==="esarthee"?"selected":""}`} onClick={()=>setNav("esarthee")} >E-Sarthee</a>
-              <a href="https://p-tunes.vercel.app/"  className={`second-nav-content ${nav==="p-tunes"?"selected":""}`} onClick={()=>setNav("p-tunes")} >P-Tunes</a>
-              {/* <Link to='/contact' className={`second-nav-content ${nav==="contact"?"selected":""}`} onClick={()=>setNav("contact")} >Contact</Link> */}
-          
-        </nav>
+        <div className='second-navbar-container'>
+            <div  style={{marginTop:"5px"}}>
+                <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 4L8 12L16 20" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+           
+            <nav className="nav-second mobile-nav">
+                <Link to="/about" className={`second-nav-content ${nav==="about"?"selected":""}`} onClick={()=>setNav("about")}>About</Link>
+                <Link to='/about' className={`second-nav-content ${nav==="hello"?"selected":""}`} onClick={()=>window.open("https://ai-divie-purush-gurjar.vercel.app","_self")} >AI-Divie</Link>
+                <a href="https://esarthee.purushottam.online/"  className={`second-nav-content ${nav==="esarthee"?"selected":""}`} onClick={()=>setNav("esarthee")} >E-Sarthee</a>              
+                <a href="https://p-tunes.vercel.app/"  className={`second-nav-content ${nav==="p-tunes"?"selected":""}`} onClick={()=>setNav("p-tunes")} >P-Tunes</a>
+                <Link to='/contact' className={`second-nav-content ${nav==="contact"?"selected":""}`} onClick={()=>setNav("contact")} >Contact</Link>
+            </nav>
+
+             <div style={{marginTop:"5px"}}>
+                <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 4L16 12L8 20" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            
+
+        </div>
+
+
         <PopupAI/>
 
       </div>
